4486e1b08a9c4532ce989140604d9382d50c20db
sorious77 <sorious77@naver.com>
2023-01-16 14:50:49 +0900

    getAll, getMovieById 테스트 코드 작성

--- src/movies/movies.service.spec.ts
+++ src/movies/movies.service.spec.ts
@@ -1,3 +1,4 @@
+import { NotFoundException } from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
 import { MoviesService } from './movies.service';
 
@@ -14,5 +15,36 @@ describe('MoviesService', () => {
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  describe('getAll()', () => {
+    it('should return an array', () => {
+      const result = service.getAll();
+
+      expect(result).toBeInstanceOf(Array);
+    });
+  });
+
+  describe('getMovieById()', () => {
+    it('should return a movie', () => {
+      service.createMovie({
+        title: 'test',
+        genres: ['test'],
+        year: 2023,
+      });
+      const movie = service.getMovieById(1);
+
+      expect(movie).toBeDefined();
+      expect(movie.title).toEqual('test');
+    });
+
+    it('should throw 404 error', () => {
+      try {
+        service.getMovieById(999);
+      } catch (e) {
+        expect(e).toBeInstanceOf(NotFoundException);
+        expect(e.message).toEqual('Movie with Id(999) Not found');
+      }
+    });
   });
 });
