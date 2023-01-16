f8de3d0d41c544967ebe76ffd7a6593447babd53
sorious77 <sorious77@naver.com>
2023-01-16 15:06:08 +0900

    delete, create 테스트 코드 작성

--- src/movies/movies.service.spec.ts
+++ src/movies/movies.service.spec.ts
@@ -1,5 +1,6 @@
 import { NotFoundException } from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
+import { after } from 'node:test';
 import { MoviesService } from './movies.service';
 
 describe('MoviesService', () => {
@@ -45,6 +46,48 @@ describe('MoviesService', () => {
         expect(e).toBeInstanceOf(NotFoundException);
         expect(e.message).toEqual('Movie with Id(999) Not found');
       }
+    });
+  });
+
+  describe('deleteMovie()', () => {
+    it('delete a movie', () => {
+      service.createMovie({
+        title: 'test',
+        genres: ['test'],
+        year: 2023,
+      });
+
+      const beforeDelete = service.getAll().length;
+      const result = service.deleteMovie(1);
+      const afterDelete = service.getAll().length;
+
+      expect(afterDelete).toEqual(beforeDelete - 1);
+      expect(result).toEqual(true);
+    });
+
+    it('should throw 404 error', () => {
+      try {
+        service.deleteMovie(999);
+      } catch (e) {
+        expect(e).toBeInstanceOf(NotFoundException);
+        expect(e.message).toEqual('Movie with Id(999) Not found');
+      }
+    });
+  });
+
+  describe('createMovie()', () => {
+    it('should create a movie', () => {
+      const beforeCreate = service.getAll().length;
+
+      service.createMovie({
+        title: 'test',
+        genres: ['test'],
+        year: 2023,
+      });
+
+      const afterCreate = service.getAll().length;
+
+      expect(afterCreate).toEqual(beforeCreate + 1);
     });
   });
 });
